2f85d29f278717eecbf383228fde1e156fb119ce
Gaston Bauer <gaston.bauer@cencosud.com.uy>
2023-08-30 19:26:01 -0300

    creando read

---
+++ src/Library/Read.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace PII_Game_Of_Life
+{
+  public class Read
+  {
+    private string url = @"../../assets/board.txt";
+    private string[] contentLines;
+
+    public Read()
+    {
+      contentLines = ReadFile(); // Inicialización en el constructor
+      bool[,] board = new bool[contentLines.Length, contentLines[0].Length];
+
+
+      for (int y = 0; y < contentLines.Length; y++)
+      {
+        for (int x = 0; x < contentLines[y].Length; x++)
+        {
+          if (contentLines[y][x] == '1')
+          {
+            board[x, y] = true;
+          }
+        }
+      }
+
+      System.Console.WriteLine(board);
+
+
+    }
+
+    public string[] ReadFile()
+    {
+      string[] lines = File.ReadAllLines(url);
+      return lines;
+    }
+
+  }
+}
